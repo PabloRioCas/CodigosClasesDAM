8eb1d1060054c5c6e4c088bdf1266014bad0a4ef
Pablo <148128366+PabloRioCas@users.noreply.github.com>
2026-08-12 18:28:32 +0200

    Delete Acc Datos/codigos2425/T5/hotelapi/usuarioapp_crud directory

--- Acc Datos/codigos2425/T5/hotelapi/usuarioapp_crud/src/main/java/com/borja/ue/usuarioapp/repos/PerfilesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.borja.ue.usuarioapp.repos;
-
-import com.borja.ue.usuarioapp.domain.Perfiles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PerfilesRepository extends JpaRepository<Perfiles, Long> {
-}
